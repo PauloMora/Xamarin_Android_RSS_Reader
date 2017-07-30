bb9fad7e66ac9235adf3f8f3affa5a4405f3556e
Paulo Mora <paulomora.90@gmail.com>
2017-07-30 20:54:17 +0100

    add v1 app

---
+++ Xamarin_Android_RSS_Reader/Xamarin_Android_RSS_Reader/Adapter/FeedAdapter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using Xamarin_Android_RSS_Reader.Interface;
+using Xamarin_Android_RSS_Reader.Model;
+
+namespace Xamarin_Android_RSS_Reader.Adapter
+{
+    class FeedViewHolder : RecyclerView.ViewHolder, View.IOnClickListener, View.IOnLongClickListener
+    {
+
+        public TextView txtTitle, txtContent, txtPubDate;
+        public ItemClickListener ItemClickListener { get; set; }
+
+        public FeedViewHolder(View itemView):base(itemView)
+        {
+            txtTitle = (TextView)itemView.FindViewById(Resource.Id.txtTitle);
+            txtPubDate = (TextView)itemView.FindViewById(Resource.Id.txtPubDate);
+            txtContent = (TextView)itemView.FindViewById(Resource.Id.txtContent);
+
+            //set event
+            itemView.SetOnClickListener(this);
+            itemView.SetOnLongClickListener(this);
+
+
+        }
+
+        public void OnClick(View v)
+        {
+            ItemClickListener.OnClick(v, AdapterPosition, false);
+        }
+
+        public bool OnLongClick(View v)
+        {
+            ItemClickListener.OnClick(v, AdapterPosition, true);
+            return true;
+        }
+    }
+
+    public class FeedAdapter : RecyclerView.Adapter, ItemClickListener
+    {
+        private RssObject rssObject;
+        private Context mContext;
+        private LayoutInflater inflater;
+
+        public FeedAdapter(RssObject rssObeject, Context mContext)
+        {
+            this.rssObject = rssObeject;
+            this.mContext = mContext;
+            inflater = LayoutInflater.From(mContext);
+        }
+
+        public override int ItemCount => rssObject.items.Count;
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            FeedViewHolder hh = holder as FeedViewHolder;
+            hh.txtTitle.Text = rssObject.items[position].title;
+            hh.txtPubDate.Text = rssObject.items[position].pubDate;
+            hh.txtContent.Text = rssObject.items[position].content;
+
+            hh.ItemClickListener = this;
+
+
+        }
+
+        public void OnClick(View view, int position, bool isLongCLick)
+        {
+            if (!isLongCLick)
+            {
+                Intent intent = new Intent(Intent.ActionView, Android.Net.Uri.Parse(rssObject.items[position].link));
+                mContext.StartActivity(intent);
+            }
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = inflater.Inflate(Resource.Layout.Row, parent, false);
+            return new FeedViewHolder(itemView);
+        }
+    }
+}
---
+++ Xamarin_Android_RSS_Reader/Xamarin_Android_RSS_Reader/MainActivity.cs
@@ -0,0 +1,100 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Xamarin_Android_RSS_Reader.Model;
+using System;
+using Xamarin_Android_RSS_Reader.Common;
+using Java.Lang;
+using Newtonsoft.Json;
+using Xamarin_Android_RSS_Reader.Adapter;
+using Android.Views;
+
+namespace Xamarin_Android_RSS_Reader
+{
+    [Activity(Label = "Xamarin_Android_RSS_Reader", MainLauncher = true, Icon = "@drawable/icon",Theme ="@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+
+        Android.Support.V7.Widget.Toolbar toolbar;
+        RecyclerView recyclreView;
+        RssObject rssObject;
+
+        private const string RSS_Link = "http://rss.nytimes.com/services/xml/rss/nyt/Science.xml";
+        private const string RSS_to_json = "https://api.rss2json.com/v1/api.json?rss_url=";
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.Main_Menu, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (item.ItemId == Resource.Id.menu_refresh) ;
+                LoadData();
+            return true;
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+
+            toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            toolbar.Title = "News";
+            SetSupportActionBar(toolbar);
+
+            recyclreView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.Vertical, false);
+            recyclreView.SetLayoutManager(linearLayoutManager);
+
+            LoadData();
+
+        }
+
+        private void LoadData()
+        {
+            StringBuilder strBuilder = new StringBuilder(RSS_to_json);
+            strBuilder.Append(RSS_Link);
+
+            new LoadDataAsync(this).Execute(strBuilder.ToString());
+        }
+
+        class LoadDataAsync : AsyncTask<string, string, string>
+        {
+            MainActivity mainActivity;
+            ProgressDialog mDialog;
+
+            public LoadDataAsync (MainActivity mainActivity)
+            {
+                this.mainActivity = mainActivity;
+            }
+
+            protected override void OnPreExecute()
+            {
+                mDialog = new ProgressDialog(mainActivity);
+                mDialog.Window.SetType(Android.Views.WindowManagerTypes.SystemAlert);
+                mDialog.SetMessage("Please wait...");
+                mDialog.Show();
+            }
+
+            protected override string RunInBackground(params string[] @params)
+            {
+                string result = new HTTPDataHandler().GetHTTPData(@params[0]);
+                return result;
+            }
+
+            protected override void OnPostExecute(string result)
+            {
+                RssObject data = JsonConvert.DeserializeObject<RssObject>(result);
+                mDialog.Dismiss();
+                FeedAdapter adapter = new FeedAdapter(data, mainActivity);
+                mainActivity.recyclreView.SetAdapter(adapter);
+                adapter.NotifyDataSetChanged();
+            }
+        }
+    }
+}
+
